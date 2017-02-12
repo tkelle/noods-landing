49cf73e723338179ece5e1865af3f218ad3e4080
taavi <taavet.kelle@gmail.com>
2017-02-12 17:03:46 +0100

    new stuff

---
+++ index.php
@@ -0,0 +1,187 @@
+<?php include 'header.php';?>
+
+<!-- Content begins -->
+
+<!-- Main container -->
+<div class="mainContainer">
+	<!-- Inner container for content -->
+	<div class="innerContainer">
+		<!-- Cube starts -->
+		<!-- <section class="row row--cube">
+			<div class="cubeWrap">
+				<div class="cube">
+					<div class="front">
+						<img src="http://lorempixel.com/200/200/animals/" alt="">
+					</div>
+					<div class="back">
+						<img src="http://lorempixel.com/200/200/animals/" alt="">
+					</div>
+					<div class="top">
+						<img src="http://lorempixel.com/200/200/animals/" alt="">
+					</div>
+					<div class="bottom">
+						<img src="http://lorempixel.com/200/200/animals/" alt="">
+					</div>
+					<div class="left">
+						<img src="http://lorempixel.com/200/200/animals/" alt="">
+					</div>
+					<div class="right">
+						<img src="http://lorempixel.com/200/200/animals/" alt="">
+					</div>
+				</div>
+			</div>
+		</section> -->
+		<!-- Cube ends -->
+		<!-- Slider section -->
+		<section class="row row--right row--slider">
+			<h1 class="title title--medium title--rowTitle">Highlights</h1>
+			<div class="slide">
+				<div class="slide__background">
+					<img src="img/bibio.jpg" alt="" class="slide__image">
+				</div>
+				<div class="slide__content">
+					<h2 class="title title--small title--slide">
+						Testing
+					</h2>
+					<p class="slide__copy">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus, perspiciatis?
+					</p>
+				</div>
+			</div>	
+		</section>
+		<!-- Slider section ends -->
+		<!-- Latest shows -->
+		<section class="row row--left row--shows">
+			<h1 class="title title--medium title--rowTitle">Latest shows</h1>
+			<div class="tile">
+				<div class="tile__sizer">
+					<div class="tile__background">
+						<img src="img/bibio.jpg" alt="" class="tile__image">
+					</div>
+					<div class="tile__content">
+						<h2 class="title title--small title--slide">
+							Bruce
+						</h2>
+						<p class="slide__copy">
+							01.01.2017
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class="tile">
+				<div class="tile__sizer">
+					<div class="tile__background">
+						<img src="img/bibio.jpg" alt="" class="tile__image">
+					</div>
+					<div class="tile__content">
+						<h2 class="title title--small title--slide">
+							Crosspolar w/ Sangam
+						</h2>
+						<p class="slide__copy">
+							23.12.2016
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class="tile">
+				<div class="tile__sizer">
+					<div class="tile__background">
+						<img src="img/bibio.jpg" alt="" class="tile__image">
+					</div>
+					<div class="tile__content">
+						<h2 class="title title--small title--slide">
+							Team w/ Luke G
+						</h2>
+						<p class="slide__copy">
+							30.10.16
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class="tile">
+				<div class="tile__sizer">
+					<div class="tile__background">
+						<img src="img/bibio.jpg" alt="" class="tile__image">
+					</div>
+					<div class="tile__content">
+						<h2 class="title title--small title--slide">
+							Team w/ Luke G
+						</h2>
+						<p class="slide__copy">
+							30.10.16
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class="tile tile--viewAll">
+				<div class="tile__sizer tile__sizer--viewAll">
+					<div class="tile__content">
+						<h2 class="title title--slide title--viewAll">
+							View more
+						</h2>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- Latest shows ends -->
+		<!-- Upcoming event -->
+		<section class="row row--right row--event">
+			<div class="eventWrapper">
+				<div class="poster poster--image">
+					<img src="img/poster.jpg" alt="" class="poster__image">
+				</div>
+				<div class="poster poster--metadata">
+					<h1 class="title title--medium">Coming up</h1>
+					<h2 class="event__title">
+						New Years Day: Sunday Seshwan
+					</h2>
+					<p class="event__date">
+						<span class="event__date--date">01.01.2017</span>
+						<span class="event__date--time">22:00—Late</span>
+					</p>
+					<p class="event__artists">
+						<span>Artist Name 1</span>
+						<span>Artist Name 2</span>
+						<span>Noods Residents</span>
+					</p>
+					<p class="event__location">
+						The Surrey Vaults
+					</p>
+					<p class="event__price">
+						£6
+					</p>
+					<p class="event__description">
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi laborum optio, amet doloremque architecto voluptatum consectetur fugit delectus accusamus tenetur!
+					</p>
+					<a href="#" class="event__link event__link--info">
+						More info
+					</a>
+					<a href="#" class="event__link event__link--tickets">
+						Buy tickets
+					</a>
+				</div>
+			</div>
+		</section>
+		<!-- Upcoming events ends -->
+		<!-- Store starts -->
+		<section class="row row--left row--shop">
+			<h1 class="title title--medium title--rowTitle">Store</h1>
+			<div class="slide slide--store">
+				<div class="slide__background">
+					<img src="img/shop1.jpg" alt="" class="slide__image">
+				</div>
+				<div class="slide__content">
+					<h2 class="title title--slide title--medium">
+						Visit store
+					</h2>
+				</div>
+			</div>
+		</section>
+		<!-- Store ends -->
+	</div>
+	<!-- Inner container for content ends -->
+</div>
+
+<!-- Content ends -->
+
+<?php include 'footer.php';?>
--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,42 @@
-$(document).ready(function() {
-	$('#bgScene').parallax();
-	$('#contentScene').parallax();
-});
+var bgElement = document.getElementsByClassName('randomBgImage');
+var bodyWidth = $(document).width();
+var bodyHeight = $(document).height();
 
 $(window).load(function() {
-	var bgElement = document.getElementsByClassName('randomBgImage');
-
-	var bodyWidth = $(document).width();
-	var bodyHeight = $(document).height();
-
 	$.each(bgElement,function() {
-		var randPosX = Math.floor((Math.random()*bodyWidth));
+		var randPosX = Math.floor((Math.random()*bodyWidth-100));
 		$(this).css('left', randPosX);
 	});
 
 	$.each(bgElement,function() {
-		var randPosY = Math.floor((Math.random()*bodyHeight));
+		var randPosY = Math.floor((Math.random()*bodyHeight-100));
 		$(this).css('top', randPosY);
 	});
 });
+
+$(window).resize(function() {
+  $.each(bgElement,function() {
+  	var randPosX = Math.floor((Math.random()*bodyWidth));
+  	$(this).css('left', randPosX);
+  });
+
+  $.each(bgElement,function() {
+  	var randPosY = Math.floor((Math.random()*bodyHeight));
+  	$(this).css('top', randPosY);
+  });
+});
+
+$(document).ready(function() {
+	$('#bgScene').parallax();
+	$('#contentScene').parallax();
+
+	$('.mobileNavTrigger').click(function() {
+		// $('.bgShader').toggleClass("bgShader--active"),
+		$('.bgShader').fadeToggle( "slow", "linear" ),
+		// $('.navContainer').toggleClass("navContainer--active");
+		$('.mobileNavTrigger').toggleClass("mobileNavTrigger--active"),
+		$('.navContainer').fadeToggle( "slow", "linear" );
+	});
+});
+
 
